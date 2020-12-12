b1ed62924270c32afdaa4e4061e5ee45de6b5e7e
Dennis <dennisilee@gmail.com>
2020-12-12 02:03:10 -0800

    save high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -100,7 +100,7 @@ var fight = function(enemy) {
       }
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
       // remove enemy's health by subtracting the amount we set in the damage variable
-      enemy.health = Math.max(0, enemy.health - damage);
+      enemy.health = Math.max(0, enemy.health - damage - 100);
       console.log(
         playerInfo.name +
           " attacked " +
@@ -193,13 +193,24 @@ var startGame = function() {
 
 // function to end the entire game
 var endGame = function() {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player have more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   } 
   else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
+
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
